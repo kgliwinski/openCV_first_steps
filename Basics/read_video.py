9c32a294385fbff41b71ae65424f020981ac2bdb
kgliwinski <259282@student.pwr.edu.pl>
2023-02-08 23:13:39 +0100

    Add rescaling

--- Basics/read_video.py
+++ Basics/read_video.py
@@ -1,20 +1,39 @@
 import cv2 as cv
 
-# Reading videos
+# rescaling frames - works on both videos(frames) and images
+def rescale_frame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)  # frame[1] - width
+    height = int(frame.shape[0] * scale)  # frame[0] - height
+    dimensions = (width, height)
 
-capture = cv.VideoCapture(0)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('Video', frame)
+# Reading and showing videos
+def video_capture(source, scale=0.75):
+    capture = cv.VideoCapture(source)
 
-    # if letter d is pressed, it will break
-    if cv.waitKey(20) & 0xFF==ord('d'): 
-        break
+    while True:
+        isTrue, frame = capture.read()
+        
+        new_frame = rescale_frame(frame)
+        
+        cv.imshow('Video', frame)
+        cv.imshow('Video_rescaled', new_frame)
 
-capture.release()
+        # if letter d is pressed, it will break
+        if cv.waitKey(20) & 0xFF == ord('d'):
+            break
 
-# assertion failed -215: it means that the video ran out of frames
-# OR the path is wrong (in case of images and videos)
+    capture.release()
 
-cv.destroyAllWindows()
+    # assertion failed -215: it means that the video ran out of frames
+    # OR the path is wrong (in case of images and videos)
+
+    cv.destroyAllWindows()
+
+# changing the resolution of live video
+def change_res(capture, width, height):
+    capture.set(3, width)
+    capture.set(4, height)
+
+video_capture(0)
